fb8cfc9788a2996148a748e58d92ad88a40e013b
Pavel.K <pavelweb74@gmail.com>
2024-09-03 20:53:56 +0500

    rename get to view in route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 123;
-    return view('welcome');
-});
+Route::view('/', 'welcome', ['User' => 'Admin']);
+
 
 Route::group([], function () {
     Route::get('/test', function () {
