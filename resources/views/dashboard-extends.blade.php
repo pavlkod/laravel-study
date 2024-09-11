e9074076d45a7ec54f175c197fc1597ec27629d8
Pavel.K <pavelweb74@gmail.com>
2024-09-11 22:19:49 +0500

    add @extends layout

---
+++ resources/views/dashboard-extends.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/layouts/master.blаdе.php. -->
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+Welcome to your application dashboard!
+@endsection
+
+@section('footerScripts')
+@parent
+<script src="dashboard.js"></script>
+@endsection
