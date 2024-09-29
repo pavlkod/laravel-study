a5215a35d0682d0c904e54e5cc1f22007d17c4e4
Pavel.K <pavelweb74@gmail.com>
2024-09-29 20:19:35 +0500

    //add global scope Post::ActiveVips()->get()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// use App\Scopes\ActiveScope;
+
 class Post extends Model
 {
     use HasFactory;
+
     //add local scope Post::ActiveVips()->get()
     public function scopeActiveVips($query)
     {
         return $query->where('vip', true)->where('trial', false);
     }
+
+    //add global scope
+    protected static function boot()
+    {
+        раrent::boot();
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('active', true);
+        });
+    }
+
+    //or
+    // static::addGlobalScope(new ActiveScope);
 }
