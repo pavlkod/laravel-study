9164ecbdf15795f38794a95916e7fe4984e3b2c3
Pavel.K <pavelweb74@gmail.com>
2024-10-16 22:31:25 +0500

    Add email:newusers in artisan

---
+++ app/Console/Commands/WelcomeNewUsers.php
@@ -0,0 +1,41 @@
+<?php
+
+// php artisan make:command WelcomeNewUsers --command=email:newusers
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class WelcomeNewUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:newusers {test?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    // inject UserMailer
+    /*     public function __construct(UserMailer $userMailer)
+        {
+            parent::__construct();
+            $this->userMailer = $userMailer;
+        } */
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        echo 234;
+        print_r($this->arguments());
+        // $this->userMailer->welcomeNewUsers();
+    }
+}
