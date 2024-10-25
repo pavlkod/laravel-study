72d31b6c9dc840d0a404a2877d013239346aaefd
Pavel.K <pavelweb74@gmail.com>
2024-10-25 21:53:56 +0500

    add "password:reset {userld} {--sendEmail}" Artisan command in routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command(
+    'password:reset {userld} {--sendEmail}',
+    function ($userld, $sendEmail) {
+        $userld = $this->argument('userld');
+        // some code
+    }
+);
