e2187e60ac41ca09cb5b695928d49b6552b01469
Pavel.K <pavelweb74@gmail.com>
2024-11-15 22:28:53 +0500

    include auth route in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Process;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    });
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,3 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
