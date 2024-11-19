56992268d70045cf9f56f98f79ff8ffe96a66eaa
Pavel.K <pavelweb74@gmail.com>
2024-11-19 20:32:05 +0500

    add resources/lang/messages.php

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'task-deletion' => 'You have deleted а task|You have successfully deleted tasks',
+    'task-deletion2' => '{0} You didn\'t manage to delete апу tasks.|[1,4] You deleted а few tasks.|[5,Inf] You deleted а whole ton of tasks.',
+];
