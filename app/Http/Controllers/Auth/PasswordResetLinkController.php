a9598256d3c042c9465c0e5232bd28b6ee5d80a8
Pavel.K <pavelweb74@gmail.com>
2024-11-30 15:20:20 +0500

    add PasswordResetLinkController@create

---
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\View\View;
+
+class PasswordResetLinkController extends Controller
+{
+    /**
+     * Display the password reset link request view.
+     */
+    public function create(): View
+    {
+        return view('auth.forgot-password');
+    }
+}
