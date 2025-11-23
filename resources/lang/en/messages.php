1c0b073a4f0fa264ef615a378c4abb9ce44c7a11
Pavel.K <pavelweb74@gmail.com>
2025-11-23 16:08:50 +0500

    add dynamic text in lang\en\messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,4 +3,5 @@
 return [
     'task-deletion' => 'You have deleted а task|You have successfully deleted tasks',
     'task-deletion2' => '{0} You didn\'t manage to delete апу tasks.|[1,4] You deleted а few tasks.|[5,Inf] You deleted а whole ton of tasks.',
+    'task-deletion3' => '{0} You didn\'t manage to delete апу tasks.|[1,4] You deleted а few tasks.|[5,Inf] You deleted а whole ton of tasks with :section.',
 ];
