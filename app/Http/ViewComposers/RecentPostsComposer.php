71f3decc60ccca12a423e78deb71b21c984a513c
Pavel.K <pavelweb74@gmail.com>
2026-03-29 14:45:40 +0500

    add User in RecentPostsComposer->constructor

--- app/Http/ViewComposers/RecentPostsComposer.php
+++ app/Http/ViewComposers/RecentPostsComposer.php
@@ -1,12 +1,21 @@
-<?
+<?php
 
 namespace App\Http\ViewComposers;
 
+use App\Models\User;
 use App\Post;
 use Illuminate\Contracts\View\View;
 
 class RecentPostsComposer
 {
+    /**
+     * Create a new profile composer.
+     */
+    public function __construct(
+        protected User $users,
+    ) {
+    }
+
     public function compose(View $view)
     {
         $view->with('recentPosts', Post::recent());
