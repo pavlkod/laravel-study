94940d4521299a3ae26dbf25ed142aff28b8a7b6
Pavel.K <pavelweb74@gmail.com>
2026-04-01 22:47:48 +0500

    add return Resources for Route::get('/post/{postId}') in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Resources;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,6 +18,10 @@ Route::view('/', 'welcome');
 Route::get('/', function(){
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/post/{postId}', function($postId) {
+    return new Resources\Post(Post::find($postId));
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
